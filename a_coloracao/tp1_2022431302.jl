16337232f7e6b443f912b7d7474be8cc720d0d12
lucaswnn <lucaswnn@hotmail.com>
2025-09-23 10:15:03 -0300

    correção a-coloração e testes atualizados

--- a_coloracao/tp1_2022431302.jl
+++ a_coloracao/tp1_2022431302.jl
@@ -3,12 +3,14 @@ using Gurobi
 
 mutable struct ProblemData
     n::Int
-    arestas::Set{Tuple{Int, Int}}
+    arestas::Set{Tuple{Int,Int}}
+    arestas_aux::Set{Tuple{Int,Int}}
 end
 
 function readData(file)
     n = 0
     arestas = Set()
+    arestas_aux = Set()
     for l in eachline(file)
         q = split(l)
         if q[1] == "n"
@@ -17,9 +19,11 @@ function readData(file)
             v1 = parse(Float64, q[2])
             v2 = parse(Float64, q[3])
             push!(arestas, (v1, v2))
+            push!(arestas_aux, (v1, v2))
+            push!(arestas_aux, (v2, v1))
         end
     end
-    return ProblemData(n, arestas)
+    return ProblemData(n, arestas, arestas_aux)
 end
 
 file = open(ARGS[1], "r")
@@ -39,48 +43,65 @@ set_attribute(model, "OutputFlag", 0)
 # zc1c2 = 1 se os vértices com a cor c1 tem vizinho com a cor c2
 @variable(model, z[c1=1:data.n, c2=1:data.n], Bin)
 
+@variable(model, t[(u, v) in data.arestas_aux, c1=1:data.n, c2=1:data.n], Bin)
+
 # vértices só podem ter uma cor
-for i in 1:data.n
-    @constraint(model, sum(x[i, c] for c = 1:data.n) == 1)
+for v in 1:data.n
+    @constraint(model, sum(x[v, c] for c = 1:data.n) == 1)
 end
 
 # vértices conectados não podem ter a mesma cor
 for c in 1:data.n
-    for (u,v) in data.arestas
+    for (u, v) in data.arestas
         @constraint(model, x[u, c] + x[v, c] <= y[c])
     end
 end
 
-# determina se a cor i tem vizinho com a cor j
+# y[(u,v), c1, c2] = x[u,c1] * x[v,c2]
 for c1 in 1:data.n
     for c2 in 1:data.n
         if c1 != c2
-            for (u,v) in data.arestas
-                @constraint(model, x[u,c1] + x[v,c2] - 1 <= z[c1,c2])
-                @constraint(model, x[u,c2] + x[v,c1] - 1 <= z[c1,c2])
+            for (u, v) in data.arestas_aux
+                @constraint(model, t[(u, v), c1, c2] <= x[u, c1])
+                @constraint(model, t[(u, v), c1, c2] <= x[v, c2])
+                @constraint(model, t[(u, v), c1, c2] >= x[u, c1] + x[v, c2] - 1)
             end
         end
     end
 end
 
-# verifica se nenhum vértice com cor i tem como vizinho a cor j
-for c1 in 1:data.n
-    for c2 in 1:data.n
-        @constraint(model, z[c1,c2] <= sum(1 - (x[u,c1] - x[v,c2]) for (u,v) in data.arestas))
-    end
-end
-
-# cor i possui todos vizinhos com cor j
+# se vértices ligados tem cores diferentes, z[c1,c2] = 1
 for c1 in 1:data.n
     for c2 in 1:data.n
         if c1 != c2
-            @constraint(model, z[c1,c2] >= y[c1] + y[c2] - 1)
+            for (u, v) in data.arestas_aux
+                @constraint(model, z[c1, c2] >= t[(u, v), c1, c2])
+                @constraint(model, z[c2, c1] >= t[(u, v), c2, c1])
+            end
         end
     end
 end
 
-# Função objetivo: minimizar o número de cores usadas
-@objective(model, Min, sum(y[i] for i = 1:data.n))
+# se não existe ligação entre vértices com cores c1 e c2, z[c1,c2] = 0
+for c1 in 1:data.n
+    for c2 in 1:data.n
+        if c1 != c2
+            @constraint(model, z[c1, c2] <= sum(t[(u, v), c1, c2] for (u, v) in data.arestas_aux))
+            @constraint(model, z[c2, c1] <= sum(t[(u, v), c2, c1] for (u, v) in data.arestas_aux))
+        end
+    end
+end
+
+for c1 in 1:data.n
+    for c2 in 1:data.n
+        if c1 != c2
+            @constraint(model, z[c1, c2] >= y[c1] + y[c2] - 1)
+        end
+    end
+end
+
+# Função objetivo: maximizar o número de cores usadas
+@objective(model, Max, sum(y[i] for i = 1:data.n))
 
 # Resolve o modelo
 optimize!(model)
